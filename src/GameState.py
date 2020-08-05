936f9ecf5c38ff7d515cfb43d4d27a3d3d4e538a
jojrg <jochen.joerg@gmail.com>
2020-08-06 00:55:28 +0200

    minor fixes

--- src/GameState.py
+++ src/GameState.py
@@ -1,6 +1,3 @@
-from enum import Enum
-
-
 class GameState:
     NEW = 0
     STARTED = 1
--- src/Guess.py
+++ src/Guess.py
@@ -10,15 +10,23 @@ class Guess:
         :foo: 
     """
 
-    def __init__(self, value, distance):
+    def __init__(self, val, dist):
         super().__init__()
-        self.__value = value
-        self.__distance = distance
+        self.value = val
+        self.distance = dist
 
     @property
     def value(self):
-        return self.__value[:]
+        return self.__value
+
+    @value.setter
+    def value(self, val):
+        self.__value = val
 
     @property
     def distance(self):
-        return self.__distance[:]
+        return self.__distance
+
+    @distance.setter
+    def distance(self, val):
+        self.__distance = val
